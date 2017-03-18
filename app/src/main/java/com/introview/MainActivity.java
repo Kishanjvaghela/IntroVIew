08549bde4989738604cc0c1deca9a36bd9b20537
Kishan <kishan.j.vaghela@gmail.com>
2017-03-18 20:32:35 +0530

    add data binding

--- app/src/main/java/com/introview/MainActivity.java
+++ app/src/main/java/com/introview/MainActivity.java
@@ -1,29 +1,29 @@
 package com.introview;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import com.introlayout.IntroView;
+import com.introview.databinding.ActivityMainBinding;
 
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView titleView;
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        titleView = (TextView) findViewById(R.id.titleView);
-
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         new IntroView.Builder(this)
                 .setDelayMillis(500)
                 .enableFadeAnimation(true)
                 .performClick(true)
                 .setInfoText("Hi There! Click this card and see what happens.")
-                .setTarget(titleView)
+                .setTarget(mainBinding.titleView)
                 .setUsageId(UUID.randomUUID().toString()) //THIS SHOULD BE UNIQUE ID
                 .show();
 
